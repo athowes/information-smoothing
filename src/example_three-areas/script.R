01f7e432afe7887b62df227a2e0e78c0ac8e4ebd
Adam Howes <adamhowes@outlook.com>
2021-10-11 17:16:40 +0100

    Start orderly, rename to information-smoothing

---
+++ src/example_three-areas/script.R
@@ -0,0 +1,5 @@
+#' Uncomment and run the two line below to resume development of this script
+# orderly::orderly_develop_start("three-areas")
+# setwd("src/three-areas")
+
+rmarkdown::render("three-areas.Rmd", "pdf_document")
