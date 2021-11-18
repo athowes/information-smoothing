52243a53f52dec267342f631318832882b865bc3
Adam Howes <adamhowes@outlook.com>
2021-11-18 17:55:53 +0000

    Clean up

--- src/example_three-areas/script.R
+++ src/example_three-areas/script.R
@@ -1,5 +1,5 @@
 #' Uncomment and run the two line below to resume development of this script
-# orderly::orderly_develop_start("three-areas")
-# setwd("src/three-areas")
+# orderly::orderly_develop_start("example_three-areas")
+# setwd("src/example_three-areas")
 
 rmarkdown::render("three-areas.Rmd", "pdf_document")
